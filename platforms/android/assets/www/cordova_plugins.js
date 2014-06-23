bb78963a986e18b8400cdd57165278e9906ef495
Eddie Ng <wards.ng@gmail.com>
2014-06-23 11:37:26 -0400

    Initial commit

---
+++ platforms/android/assets/www/cordova_plugins.js
@@ -0,0 +1,40 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+module.exports = [
+    {
+        "file": "plugins/com.ionic.keyboard/www/keyboard.js",
+        "id": "com.ionic.keyboard.keyboard",
+        "clobbers": [
+            "cordova.plugins.Keyboard"
+        ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.device/www/device.js",
+        "id": "org.apache.cordova.device.device",
+        "clobbers": [
+            "device"
+        ]
+    },
+    {
+        "file": "plugins/com.chariotsolutions.nfc.plugin/www/phonegap-nfc.js",
+        "id": "com.chariotsolutions.nfc.plugin.NFC",
+        "runs": true
+    },
+    {
+        "file": "plugins/com.phonegap.plugins.PushPlugin/www/PushNotification.js",
+        "id": "com.phonegap.plugins.PushPlugin.PushNotification",
+        "clobbers": [
+            "PushNotification"
+        ]
+    }
+];
+module.exports.metadata = 
+// TOP OF METADATA
+{
+    "com.ionic.keyboard": "0.0.1",
+    "org.apache.cordova.console": "0.2.9",
+    "org.apache.cordova.device": "0.2.10",
+    "com.chariotsolutions.nfc.plugin": "0.5.0",
+    "com.phonegap.plugins.PushPlugin": "2.2.0"
+}
+// BOTTOM OF METADATA
+});
